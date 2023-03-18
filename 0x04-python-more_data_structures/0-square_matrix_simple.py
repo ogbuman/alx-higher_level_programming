ea14b863efcc3e3a96fa52a347e20c840e722329
ogbuman <ogbuman@gamil.com>
2023-03-17 18:35:24 -0700

    updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 
-def search_replace(my_list, search, replace):
+def square_matrix_simple(matrix=[]):
     """
-    A function that replaces all occurrences
-    of an element by another in a new list
+    wordA function that computes the square
+    value of all integers of a matrix.
     """
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-    return new_list
+    new_matrix = []
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
+    return new_matrix
