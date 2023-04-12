0d13cd8fb0c6fcc91df96416e678f3bf4dbddf31
ogbuman <ogbuman@gamil.com>
2023-04-11 19:48:07 -0700

    wella

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """Defines an object attribute lookup function."""
 
 
 def lookup(obj):
     """Return a list of an objects available attributes.
     return (dir(obj))
+
